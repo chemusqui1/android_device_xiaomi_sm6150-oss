ac238361916a824f9445fc4d2600a7ebfa8a68be
Henrique Pereira <hlcpereira@pixelexperience.org>
2020-07-10 19:13:07 -0300

    sm6150-common: Add tucana to the guards
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>
    Change-Id: I64f4eae4b0e62ff35420242dd510cdb81b6a7d5a

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -24,7 +24,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "davinci phoenix violet"
+write_headers "davinci phoenix tucana violet"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
