7e94ee0222c68762c0bd1dca7b572ea65a61f319
ArixElo <patroxgamer@gmail.com>
2021-02-13 21:40:35 +0000

    sm6150-common: Drop SoundTrigger HAL [1/2]
    Signed-off-by: ArixElo <patroxgamer@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -74,7 +74,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio@6.0-impl \
-    android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
     libaudio-resampler \
     libhdmiedid \
