7d5d00049f07d54316df25ceb9e034d19e9fb518
alibei <sebastiankotzias83@gmail.com>
2021-03-20 20:17:22 +0100

    sm6150-common: parts: Remove popup camera
    Thx @chemusqui1

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -24,7 +24,6 @@ import android.util.Log;
 
 import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.doze.DozeUtils;
-import org.lineageos.settings.popupcamera.PopupCameraUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
@@ -38,6 +37,5 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         DiracUtils.initialize(context);
         DozeUtils.checkDozeService(context);
         ThermalUtils.startService(context);
-        PopupCameraUtils.startService(context);
     }
 }
