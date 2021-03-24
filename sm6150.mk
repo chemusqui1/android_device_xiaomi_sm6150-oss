ff217083e86fba720b5276b765b828d3c4b5ea14
chemusqui1 <jmportos@gmail.com>
2021-03-24 17:20:29 +0100

    sm6150-common: Build wifi-mac-generator

--- sm6150.mk
+++ sm6150.mk
@@ -394,6 +394,9 @@ PRODUCT_PACKAGES += \
     wpa_supplicant.conf \
     WifiOverlay
 
+PRODUCT_PACKAGES +=\
+    wifi-mac-generator
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
