f6d679f16ed0f99337c9870885938ce1eb9b2392
tesla59 <nishant.singh@pixysos.com>
2021-04-30 16:15:17 +0800

    sm6150-common: Uprev RIL interfaces
    * Build android.hardware.radio.deprecated
    * Uprevision android.hardware.radio.config

--- sm6150.mk
+++ sm6150.mk
@@ -338,7 +338,8 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio.config@1.1 \
+    android.hardware.radio.config@1.2 \
+    android.hardware.radio.deprecated@1.0 \
     android.hardware.radio@1.5 \
     android.hardware.secure_element@1.1 \
     libril \
