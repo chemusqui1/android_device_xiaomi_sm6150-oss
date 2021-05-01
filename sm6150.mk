571963385a1ae7f1dbbbb38452dc8f07410b5d24
alibei <sebastiankotzias83@gmail.com>
2021-05-01 06:00:19 +0200

    sm6150-common: Update bluetooth stack
    to work with newer caf blobs

--- sm6150.mk
+++ sm6150.mk
@@ -118,9 +118,10 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
-    audio.bluetooth.default \
+    com.qualcomm.qti.bluetooth_audio@1.0.vendor \
+    libbtconfigstore \
     libbthost_if \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
